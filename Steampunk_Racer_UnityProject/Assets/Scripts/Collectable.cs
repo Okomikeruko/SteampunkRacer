a742191e8df02201ec95a920f0ef1283daa5f236
Lee Whittaker <whittakerlee81@gmail.com>
2014-04-06 09:54:52 -0600

    Built and connected the system for collectables.

---
+++ Steampunk_Racer_UnityProject/Assets/Scripts/Collectable.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collectable : MonoBehaviour {
+
+	[SerializeField] 
+	private string type;
+	[SerializeField] 
+	private int value;
+
+	void OnTriggerEnter(Collider collider)
+	{
+		if (collider.tag == "Player")
+		{
+			GameObject.Find("Data").GetComponent<GameData>().collect(type, value);
+			Destroy(this);
+		}
+	}
+
+	void OnMouseDown()
+	{
+		GameObject.Find("Data").GetComponent<GameData>().collect(type, value);
+		Destroy(gameObject);
+	}
+
+}
--- Steampunk_Racer_UnityProject/Assets/Scripts/GameData.cs
+++ Steampunk_Racer_UnityProject/Assets/Scripts/GameData.cs
@@ -8,11 +8,29 @@ public class GameData : MonoBehaviour {
 
 	public int Playcount = 5;
 
+	private string[] collectableType;
+	private int[] collectableValue;
+
 	// This is an unnessary variable. We're not using it.
 	public bool test = true;
 
 	void Awake(){
 		DontDestroyOnLoad(this);
 		Application.LoadLevel ("menus");
+
+		collectableType = new string[] {"Coin"};
+		collectableValue = new int[collectableType.Length];
+	}
+
+	public void collect(string type, int value)
+	{
+		for(int i = 0; i < collectableValue.Length; i++)
+		{
+			if (collectableType[i] == type)
+			{
+				collectableValue[i] += value;
+				Debug.Log (collectableValue[i].ToString());
+			}
+		}
 	}
 }
