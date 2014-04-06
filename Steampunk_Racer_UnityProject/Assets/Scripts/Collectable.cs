a3dca4981ee36b1848eb2d7ec4b30547fdb65a10
Lee Whittaker <whittakerlee81@gmail.com>
2014-04-06 16:25:29 -0600

    Added Particle bomb to "coin" collection

--- Steampunk_Racer_UnityProject/Assets/Scripts/Collectable.cs
+++ Steampunk_Racer_UnityProject/Assets/Scripts/Collectable.cs
@@ -7,6 +7,8 @@ public class Collectable : MonoBehaviour {
 	private string type;
 	[SerializeField] 
 	private int value;
+	[SerializeField]
+	private Transform emitter;
 
 	void OnTriggerEnter(Collider collider)
 	{
@@ -20,7 +22,9 @@ public class Collectable : MonoBehaviour {
 	void OnMouseDown()
 	{
 		GameObject.Find("Data").GetComponent<GameData>().collect(type, value);
+		Object bomb = Instantiate(emitter, transform.position, Quaternion.identity);
 		Destroy(gameObject);
+		Destroy(bomb, 0.5f);
 	}
 
 }
